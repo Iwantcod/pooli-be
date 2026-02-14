c0bcfb7e9dbcfdf0e7bac9bb6fbcb976b62cc9ab
김준하 <kimjoonha0426@gmail.com>
2026-02-14 17:41:55 +0900

    init

---
+++ src/main/java/com/pooli/PooliApplication.java
@@ -0,0 +1,13 @@
+package com.pooli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PooliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pooli/PooliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pooli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
