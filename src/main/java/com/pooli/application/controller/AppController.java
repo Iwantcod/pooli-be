8139628a0098000cc51bf1aa5e663e5ee6824d4c
Misu0616 <cyniaa@naver.com>
2026-02-21 13:39:02 +0900

    remove/chore(application): 불필요한 파일, import 삭제

--- src/main/java/com/pooli/application/controller/AppController.java
+++ src/main/java/com/pooli/application/controller/AppController.java
@@ -1,13 +1,10 @@
 package com.pooli.application.controller;
 
-import java.time.LocalDateTime;
-
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pooli.application.domain.dto.response.AppResDto;
