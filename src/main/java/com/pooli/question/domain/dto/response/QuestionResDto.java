8d68a4218df07f2dc5b474cd25beedef2f17beef
Misu0616 <cyniaa@naver.com>
2026-02-21 17:36:44 +0900

    feat(etc.): coderabbit yaml 추가

---
+++ src/main/java/com/pooli/question/domain/dto/response/QuestionResDto.java
@@ -0,0 +1,20 @@
+package com.pooli.question.domain.dto.response;
+
+import com.pooli.notification.domain.dto.response.UnreadCountsResDto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "사용자 문의사항 목록 응답 DTO")
+public class QuestionResDto {
+
+	@Schema(description = "미읽음 개수", example = "15")
+	private Long unreadCount;
+}
