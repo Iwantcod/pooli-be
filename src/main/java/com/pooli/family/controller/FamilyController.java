14d2b47175e958f183ea17129a5f60951d12e5d8
ms-0o0 <201900703@hufs.ac.kr>
2026-02-21 15:01:45 +0900

    refactor(family): 브랜치를 바꾸기 위한 임시 머지

--- src/main/java/com/pooli/family/controller/FamilyController.java
+++ src/main/java/com/pooli/family/controller/FamilyController.java
@@ -1,8 +1,10 @@
 package com.pooli.family.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Family", description = "가족 관련 API")
 @RestController
 @RequestMapping("/api/families")
 public class FamilyController {
