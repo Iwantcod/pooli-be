34b18d031e2d292b91ee801b5038f3279ac1094a
김준하 <kimjoonha0426@gmail.com>
2026-02-22 23:10:58 +0900

    Feat(family): 특정 구성원의 특정 월 데이터 사용 세부 정보 조회 API Swagger

--- src/main/java/com/pooli/family/controller/FamilyController.java
+++ src/main/java/com/pooli/family/controller/FamilyController.java
@@ -1,5 +1,6 @@
 package com.pooli.family.controller;
 
+import com.pooli.family.domain.dto.response.MonthlyDataUsageDetailResDto;
 import com.pooli.family.domain.dto.response.MonthlyDataUsageResDto;
 import com.pooli.family.domain.dto.response.SharedDataThresholdResDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -74,6 +75,47 @@ public class FamilyController {
         result.add(MonthlyDataUsageResDto.builder().month(1).usage(10000).build());
         result.add(MonthlyDataUsageResDto.builder().month(2).usage(15000).build());
         result.add(MonthlyDataUsageResDto.builder().month(3).usage(9200).build());
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(
+            summary = "특정 가족 구성원의 특정 월 데이터 사용 세부 정보 조회",
+            description = "자신의 가족에 속한 구성원의 정보만 확인할 수 있습니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "조회 권한 없음"),
+            @ApiResponse(responseCode = "404", description = "자신이 속한 가족 정보가 없음"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @GetMapping("/data/detailed-usage")
+    public ResponseEntity<MonthlyDataUsageDetailResDto> getDetailedUsage(
+            @Parameter(description = "특정 가족 구성원의 회원 식별자(조회 대상자)", example = "1")
+            @RequestParam Long memberId,
+            @Parameter(description = "조회 대상 월 숫자(1~12)", example = "2")
+            @RequestParam Integer month
+    ) {
+
+        List<MonthlyDataUsageDetailResDto.DataUsageDetail> details = new ArrayList<>();
+
+        // 껍데기 데이터 삽입
+        details.add(
+                MonthlyDataUsageDetailResDto.DataUsageDetail.builder()
+                .applicationName("Youtube").totalUsageData(2100).build());
+        details.add(
+                MonthlyDataUsageDetailResDto.DataUsageDetail.builder()
+                        .applicationName("Instagram").totalUsageData(2100).build());
+        details.add(
+                MonthlyDataUsageDetailResDto.DataUsageDetail.builder()
+                        .applicationName("KakaoTalk").totalUsageData(2100).build());
+        details.add(
+                MonthlyDataUsageDetailResDto.DataUsageDetail.builder()
+                        .applicationName("Naver").totalUsageData(2100).build());
+        MonthlyDataUsageDetailResDto result = MonthlyDataUsageDetailResDto.builder()
+                .isPublic(true)
+                .dataUsageDetails(details)
+                .build();
+
         return ResponseEntity.ok(result);
     }
 }
