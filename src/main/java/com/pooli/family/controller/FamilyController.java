c5c21a56d40679eb8fe2743f6b64e3a6e1e33e56
김준하 <kimjoonha0426@gmail.com>
2026-02-22 22:42:06 +0900

    Feat(family): 특정 가족 구성원의 최근 3개월 데이터 사용량 조회 API Swagger

--- src/main/java/com/pooli/family/controller/FamilyController.java
+++ src/main/java/com/pooli/family/controller/FamilyController.java
@@ -1,5 +1,6 @@
 package com.pooli.family.controller;
 
+import com.pooli.family.domain.dto.response.MonthlyDataUsageResDto;
 import com.pooli.family.domain.dto.response.SharedDataThresholdResDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -8,6 +9,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Tag(name = "Family", description = "가족 관련 API")
 @RestController
@@ -41,7 +46,6 @@ public class FamilyController {
             @ApiResponse(responseCode = "401", description = "수정 권한 없음"),
             @ApiResponse(responseCode = "404", description = "가족 식별자에 해당하는 가족이 존재하지 않음"),
             @ApiResponse(responseCode = "500", description = "서버 오류"),
-
     })
     @PatchMapping("/shared-data-limit")
     public ResponseEntity<Void> updateSharedDataLimit(
@@ -49,5 +53,27 @@ public class FamilyController {
             @RequestParam Long familyId
     ) {
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(
+            summary = "특정 가족 구성원의 최근 3개월의 데이터 사용량 조회",
+            description = "자신의 가족에 속한 구성원의 정보만 확인할 수 있습니다. 응답 결과는 길이 3의 배열입니다. '평균 사용량'은 프론트엔드에서 연산해주세요."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "401", description = "조회 권한 없음"),
+            @ApiResponse(responseCode = "404", description = "자신이 속한 가족 정보가 없음"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @GetMapping("/data/recent-usage")
+    public ResponseEntity<List<MonthlyDataUsageResDto>> getRecentUsage(
+            @Parameter(description = "특정 가족 구성원의 회원 식별자(조회 대상자)", example = "1")
+            @RequestParam Long memberId
+    ) {
+        List<MonthlyDataUsageResDto> result = new ArrayList<>();
+        result.add(MonthlyDataUsageResDto.builder().month(1).usage(10000).build());
+        result.add(MonthlyDataUsageResDto.builder().month(2).usage(15000).build());
+        result.add(MonthlyDataUsageResDto.builder().month(3).usage(9200).build());
+        return ResponseEntity.ok(result);
     }
 }
---
+++ src/main/java/com/pooli/family/domain/dto/response/MonthlyDataUsageResDto.java
@@ -0,0 +1,15 @@
+package com.pooli.family.domain.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "월별 데이터 사용량 조회 결과 DTO")
+public class MonthlyDataUsageResDto {
+    @Schema(description = "Month를 의미하는 정수", example = "1")
+    private Integer month;
+    @Schema(description = "사용량을 의미하는 정수", example = "10000")
+    private Integer usage;
+}
