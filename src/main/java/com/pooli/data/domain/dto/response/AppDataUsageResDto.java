c31fc6ad6e626f9c764856461b6f9efc96ef2654
ms-0o0 <201900703@hufs.ac.kr>
2026-02-23 10:42:09 +0900

    document(data): 코드 컨벤션에 따른 기본 생성자 추가 및 data 송신 설명 bite 형 및 데이터 타입 Long 형으로 변경

--- src/main/java/com/pooli/data/domain/dto/response/AppDataUsageResDto.java
+++ src/main/java/com/pooli/data/domain/dto/response/AppDataUsageResDto.java
@@ -1,17 +1,18 @@
 package com.pooli.data.domain.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Schema(description = "앱 서비스별 데이터 사용량 조회 응답 DTO")
 public class AppDataUsageResDto {
 
-    @Schema(description = "총 사용 데이터량(MB)", example = "5200")
+    @Schema(description = "총 사용 데이터량(bite)", example = "5200")
     private Long totalUsedAmount;
 
     @Schema(description = "앱별 사용량 목록")
@@ -25,7 +26,7 @@ public class AppDataUsageResDto {
         @Schema(description = "애플리케이션 이름", example = "YouTube")
         private String appName;
 
-        @Schema(description = "사용량(MB)", example = "2000")
+        @Schema(description = "사용량(bite)", example = "2000")
         private Long usedAmount;
     }
 }
--- src/main/java/com/pooli/data/domain/dto/response/MonthlyDataUsageResDto.java
+++ src/main/java/com/pooli/data/domain/dto/response/MonthlyDataUsageResDto.java
@@ -1,20 +1,21 @@
 package com.pooli.data.domain.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Schema(description = "월별 데이터 사용량 조회 응답 DTO")
 public class MonthlyDataUsageResDto {
 
     @Schema(description = "월별 사용량 목록")
     private List<MonthlyUsageDto> usages;
 
-    @Schema(description = "평균 사용량(MB)", example = "1200")
+    @Schema(description = "평균 사용량(bite)", example = "1200")
     private Long averageAmount;
 
 
@@ -31,7 +32,7 @@ public class MonthlyDataUsageResDto {
         @Schema(description = "연월", example = "2026-03")
         private String yearMonth;
 
-        @Schema(description = "사용량(MB)", example = "1500")
+        @Schema(description = "사용량(bite)", example = "1500")
         private Long usedAmount;
 
     }
