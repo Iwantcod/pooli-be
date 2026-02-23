0eafcc85b081d56024aa904c1d6590eecf588bfa
ms-0o0 <201900703@hufs.ac.kr>
2026-02-23 16:36:54 +0900

    feat(user): PRIVATE 으로 타입 변경

--- src/main/java/com/pooli/user/domain/entity/Role.java
+++ src/main/java/com/pooli/user/domain/entity/Role.java
@@ -5,7 +5,7 @@ import lombok.*;
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Role {
 
     private Integer roleId;
