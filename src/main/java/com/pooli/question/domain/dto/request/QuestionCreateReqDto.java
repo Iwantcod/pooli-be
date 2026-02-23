ccc074cd20b31900b313f23e92c36ff30bb8cfd1
Misu0616 <cyniaa@naver.com>
2026-02-23 11:28:07 +0900

    feat(question): question 관련 dto, controller 생성

--- src/main/java/com/pooli/question/domain/dto/request/QuestionCreateReqDto.java
+++ src/main/java/com/pooli/question/domain/dto/request/QuestionCreateReqDto.java
@@ -1,23 +1,23 @@
-package com.pooli.question.domain.dto.response;
+package com.pooli.question.domain.dto.request;
 
-import java.time.LocalDate;
+import java.util.List;
+
+import com.pooli.question.domain.dto.QuestionAttachmentDto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@Schema(description = "사용자 문의사항 응답 DTO")
-public class QuestionResDto {
-
-	@Schema(description = "문의사항 ID", example = "1")
-	private Long questionId;
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "문의사항 생성 요청 DTO")
+public class QuestionCreateReqDto {
 	
 	@Schema(description = "문의사항 카테고리 ID", example = "2")
 	private Long questionCategoryId;
@@ -31,9 +31,6 @@ public class QuestionResDto {
 	@Schema(description = "문의사항 내용", example = "가격이 더 비싸진 것 같아요")
 	private String content;
 	
-	@Schema(description = "관리자의 답변 여부", example = "false")
-	private Boolean isAnswer;
-	
-	@Schema(description = "문의사항 생성 시점", example = "2026-02-23T14:30:00")
-	private LocalDate createdAt;
+	@Schema(description = "문의사항 첨부 파일 목록")
+	private List<QuestionAttachmentDto> attachments;
 }
--- src/main/java/com/pooli/question/domain/dto/response/QuestionListResDto.java
+++ src/main/java/com/pooli/question/domain/dto/response/QuestionListResDto.java
@@ -1,8 +1,7 @@
 package com.pooli.question.domain.dto.response;
 
-import com.pooli.notification.domain.dto.response.UnreadCountsResDto;
-
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,11 +9,24 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Schema(description = "사용자 문의사항 응답 DTO")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Schema(description = "사용자 문의사항 목록 응답 DTO")
 public class QuestionListResDto {
 
-	@Schema(description = "미읽음 개수", example = "15")
-	private Long unreadCount;
+	@Schema(description = "문의사항 ID", example = "2")
+	private Long questionId;
+	
+	@Schema(description = "문의사항 카테고리 ID", example = "3")
+	private Long questionCategoryId;
+	
+	@Schema(description = "회선 ID", example = "4")
+	private Long lineId;
+	
+	@Schema(description = "문의사항 제목", example = "데이터 한도 관련 문의드립니다")
+	private String title;
+	
+	@Schema(description = "관리자의 답변 여부", example = "false")
+	private Boolean isAnswer;
+	
 }
