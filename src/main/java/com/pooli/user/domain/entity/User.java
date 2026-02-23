aa8a63805b2f04405543c55ab43afa579e9a01a0
ms-0o0 <201900703@hufs.ac.kr>
2026-02-23 15:33:39 +0900

    feat(user): entity 작업 완료

---
+++ src/main/java/com/pooli/user/domain/entity/User.java
@@ -0,0 +1,22 @@
+package com.pooli.user.domain.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    private Long userId;
+    private String userName;
+    private String email;
+    private String password;
+    private Integer age;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/pooli/user/domain/entity/UserRole.java
@@ -0,0 +1,18 @@
+package com.pooli.user.domain.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserRole {
+
+    private Long userId;
+    private Integer roleId;
+
+    // 날짜 + 시간
+    private LocalDateTime createdAt;
+}
