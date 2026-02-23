ef2c37a6fd82e05ddda0b0566c86c6ae641b487a
hyuuuun <pam4522@gmail.com>
2026-02-23 11:50:17 +0900

    chore : import문 정리

--- src/main/java/com/pooli/question/controller/QuestionCategoryController.java
+++ src/main/java/com/pooli/question/controller/QuestionCategoryController.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.LocalDateTime;
+
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
