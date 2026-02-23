621463236f303d407aa7aea0c947ca685b501879
ms-0o0 <201900703@hufs.ac.kr>
2026-02-23 15:39:45 +0900

    feat(line): Line entity 추가

---
+++ src/main/java/com/pooli/line/domain/entity/Line.java
@@ -0,0 +1,31 @@
+package com.pooli.line.domain.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Line {
+
+    private Long lineId;
+    private Long userId;
+    private Integer planId;
+
+    private String phone;
+
+    private LocalDateTime blockEndAt;
+
+    private Long remainingData;
+
+    private Boolean isMain;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
+    private LocalDateTime updatedAt;
+
+    private Long individualThreshold;
+    private Boolean isThresholdActive;
+}
