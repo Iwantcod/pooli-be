1f92f121e7080b809afeac0587b8a59d2be98065
Misu0616 <cyniaa@naver.com>
2026-02-23 16:44:49 +0900

    feat(notification): entity 추가

---
+++ src/main/java/com/pooli/notification/domain/entity/AlarmSettingEntity.java
@@ -0,0 +1,29 @@
+package com.pooli.notification.domain.entity;
+
+import java.time.LocalDate;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AlarmSettingEntity {
+	
+	Long userId;
+	Boolean familyAlarm;
+	Boolean userAlarm;
+	Boolean policyChangeAlarm;
+	Boolean policyLimitAlarm;
+	Boolean permissionAlarm;
+	Boolean questionAlarm;
+	LocalDate createdAt;
+	LocalDate deletedAt;
+	LocalDate updatedAt;
+	
+	
+}
