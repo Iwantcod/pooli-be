350feb0d99655be9c8175456a72a8989c1e62c82
김준하 <kimjoonha0426@gmail.com>
2026-02-24 17:26:09 +0900

    Refactor(policy): 앱 정책 수정 요청 DTO의 필드명 변경

--- src/main/java/com/pooli/policy/domain/dto/request/AppPolicyUpdateValueReqDto.java
+++ src/main/java/com/pooli/policy/domain/dto/request/AppPolicyUpdateValueReqDto.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Schema(description = "사용자별 앱 정책 값(제한 데이터량 혹은 제한 속도) 수정 요청 DTO")
 public class AppPolicyUpdateValueReqDto {
     @Schema(description = "수정할 앱 정책의 식별자", example = "10")
-    private Long appDataLimitId;
+    private Long appPolicyId;
     @Schema(description = "새로운 값(제한 데이터량(Byte 단위) 혹은 제한 속도(Kbps 단위))", example = "1400")
     private Long value;
 }
