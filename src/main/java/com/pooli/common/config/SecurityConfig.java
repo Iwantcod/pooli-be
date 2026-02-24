79b94a41859bdceac07a461fcd4d01164cb900e8
hhsk908 <hhsk0908@naver.com>
2026-02-24 15:40:32 +0900

    feat(security): Session 설정 파일 생성

--- src/main/java/com/pooli/common/config/SecurityConfig.java
+++ src/main/java/com/pooli/common/config/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -76,6 +78,11 @@ public class SecurityConfig {
         repository.setCookiePath("/");
         repository.setHeaderName("X-XSRF-TOKEN");
         return repository;
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
     }
 
     @Bean
