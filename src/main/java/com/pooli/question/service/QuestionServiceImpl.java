eb075c881f39112ebf6729efeb89fcbc8261e165
ms-0o0 <201900703@hufs.ac.kr>
2026-02-25 14:33:54 +0900

    refactor(question) : dev 업데이트에 따른 임시 머지

---
+++ src/main/java/com/pooli/question/service/QuestionServiceImpl.java
@@ -0,0 +1,10 @@
+package com.pooli.question.service;
+
+import com.pooli.question.domain.dto.response.QuestionCategoryListResDto;
+
+public class QuestionServiceImpl implements QuestionService {
+    @Override
+    public QuestionCategoryListResDto getQuestionCategories() {
+        return null;
+    }
+}
