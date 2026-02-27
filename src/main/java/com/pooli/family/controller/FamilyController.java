e5395c9dbc7456951a8440c490f88d07e3d13e14
hhsk908 <hhsk0908@naver.com>
2026-02-27 11:37:05 +0900

    Feat(api) : Family 도메인 컨트롤러 서비스 레이어 연결

--- src/main/java/com/pooli/family/controller/FamilyController.java
+++ src/main/java/com/pooli/family/controller/FamilyController.java
@@ -1,27 +1,36 @@
 package com.pooli.family.controller;
 
-import com.pooli.family.domain.dto.request.UpdateSharedDataThresholdReqDto;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.pooli.family.domain.dto.request.UpdateVisibilityReqDto;
-import com.pooli.family.domain.dto.response.SharedDataThresholdResDto;
+import com.pooli.family.domain.dto.response.FamilyMembersResDto;
+import com.pooli.family.domain.dto.response.FamilyMembersSimpleResDto;
+import com.pooli.family.service.FamilyService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.pooli.family.domain.dto.response.FamilyMembersResDto;
-import com.pooli.family.domain.dto.response.FamilyMembersSimpleResDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Family", description = "가족 관련 API")
 @RestController
 @RequestMapping("/api/families")
+@RequiredArgsConstructor
 public class FamilyController {
-
+	
+	private final FamilyService familyService;
     @Operation(
             summary = "메인 대시보드 가족별 가족 구성원 정보 조회",
             description = "메인 대시보드에서 특정 가족의 상세 정보를 조회합니다. "
@@ -34,22 +43,25 @@ public class FamilyController {
                     + "당일 누적 공유풀 데이터 사용량(usageAmount)이 포함됩니다."
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "200", description = "가족별 가족 구성원 정보 조회 성공"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청 파라미터"),
             @ApiResponse(responseCode = "404", description = "가족 정보를 찾을 수 없음"),
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @GetMapping("/members")
     public ResponseEntity<FamilyMembersResDto> getFamilyMembers(
+    	  //@AuthenticationPrincipal 
+    	  //AuthUserDetails principal,
+    		@Valid @NotNull
             @Parameter(description = "가족 ID", example = "1")
-            @RequestParam Integer familyId,
-
-            @Parameter(description = "회선 ID", example = "10")
-            @RequestParam Integer lineId
+            @RequestParam 
+            Integer familyId
     ) {
+    	
+    	// 추후 Session에 저장된 principal에서 가져오는걸로 변경 예정
+    	Integer lineId = 1;
 
-        FamilyMembersResDto response = FamilyMembersResDto.builder().build();
-
+        FamilyMembersResDto response = familyService.getFamilyMembers(familyId,lineId);
         return ResponseEntity.ok(response);
     }
 
@@ -64,13 +76,16 @@ public class FamilyController {
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @GetMapping("/members-simple")
-    public ResponseEntity<FamilyMembersSimpleResDto> getFamilyMembers(
+    public ResponseEntity<List<FamilyMembersSimpleResDto>> getFamilyMembersSimple(
+    	  //@AuthenticationPrincipal AuthUserDetails principal,
             @Parameter(description = "가족 식별자", example = "1")
             @RequestParam Integer familyId
     ) {
+    	
+    	// lineId 추후 세션에서 가져올 것(@AuthenticationPrincipal)
+    	Integer lineId = 1;
 
-        FamilyMembersSimpleResDto response = FamilyMembersSimpleResDto.builder().build();
-
+        List<FamilyMembersSimpleResDto> response = familyService.getFamilyMembersSimple(familyId, lineId);
         return ResponseEntity.ok(response);
     }
 
@@ -87,9 +102,14 @@ public class FamilyController {
     })
     @PatchMapping("/visibility")
     public ResponseEntity<Void> updateVisibility(
+    	  //@AuthenticationPrincipal AuthUserDetails principal,
             @RequestBody UpdateVisibilityReqDto request
     ) {
-
+    	
+    	// lineId 추후 세션에서 가져올 것(@AuthenticationPrincipal)
+    	Integer lineId = 1;
+    	
+    	familyService.updateVisibility(lineId, request.getIsPublic());
         return ResponseEntity.ok().build();
     }
 }
