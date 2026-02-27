2be0fbec724c6f5a0f54a8f852c94c083c4ab1b6
hhsk908 <hhsk0908@naver.com>
2026-02-27 17:05:56 +0900

    Feat(api) : Data 도메인 API 각 Layer 클래스 생성

---
+++ src/main/java/com/pooli/data/service/DataService.java
@@ -0,0 +1,5 @@
+package com.pooli.data.service;
+
+public interface DataService {
+
+}
---
+++ src/main/java/com/pooli/data/service/impl/DataServiceImpl.java
@@ -0,0 +1,13 @@
+package com.pooli.data.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.pooli.data.service.DataService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DataServiceImpl implements DataService {
+
+}
