82344e99d29b5bd3d1421a034d80987e14520f71
hhsk908 <hhsk0908@naver.com>
2026-02-27 11:35:45 +0900

    Feat(api) : Family 도메인 쿼리 및 Mapper 코드 생성

---
+++ src/main/java/com/pooli/family/mapper/FamilyMapper.java
@@ -0,0 +1,41 @@
+package com.pooli.family.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.pooli.family.domain.dto.response.FamilyMembersResDto;
+import com.pooli.family.domain.dto.response.FamilyMembersSimpleResDto;
+
+@Mapper
+public interface FamilyMapper {
+	
+	FamilyMembersResDto selectFamilyMembersHeader(
+		      @Param("familyId") Integer familyId,
+		      @Param("lineId") Integer lineId
+    );
+
+	List<FamilyMembersResDto.FamilyMemberDto> selectFamilyMembers(
+        @Param("familyId") Integer familyId
+    );
+	
+	List<FamilyMembersSimpleResDto> selectFamilyMembersSimple(
+	          @Param("familyId") Integer familyId,
+	          @Param("lineId") Integer lineId);
+	
+	
+	
+	
+	Boolean isPermissionEnabledByTitle(
+	     @Param("lineId") Integer lineId,
+	     @Param("title") String title
+	);
+	
+	int updateFamilyLineVisibility(
+	    @Param("lineId") Integer lineId,
+	    @Param("isPublic") Boolean isPublic
+	);
+
+	
+}
