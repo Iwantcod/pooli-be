71d9872b67fc338f845f436c74ca6a06dbd8ca82
ms-0o0 <201900703@hufs.ac.kr>
2026-03-01 17:23:36 +0900

    feat(notification): 단일 알람 보내기 기능 추가

--- src/main/java/com/pooli/notification/domain/service/AlarmHistoryService.java
+++ src/main/java/com/pooli/notification/domain/service/AlarmHistoryService.java
@@ -2,7 +2,9 @@ package com.pooli.notification.domain.service;
 
 import com.pooli.notification.domain.enums.AlarmCode;
 
+import java.util.Map;
+
 public interface AlarmHistoryService {
-    void createAlarm(Long userId, AlarmCode alarmCode, String value);
+    void createAlarm(Long userId, AlarmCode alarmCode, Map<String, Object> values);
 
 }
--- src/main/java/com/pooli/notification/domain/service/AlarmHistoryServiceImpl.java
+++ src/main/java/com/pooli/notification/domain/service/AlarmHistoryServiceImpl.java
@@ -5,31 +5,53 @@ import com.pooli.notification.mapper.AlarmHistoryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AlarmHistoryServiceImpl implements AlarmHistoryService {
     private final AlarmHistoryMapper alarmHistoryMapper;
+    private final ObjectMapper objectMapper;
 
+    // 사용 시 type이란 키가 필수
     @Transactional
     public void createAlarm(
             Long userId,
             AlarmCode alarmCode,
-            String value
+            Map<String, Object> values
     ) {
 
-        int result = alarmHistoryMapper.insertAlarmHistory(
-                userId,
-                alarmCode.name(),
-                value
-        );
+        // 1. type 존재 여부 체크
+        if (values == null || !values.containsKey("type")) {
 
-        if (result != 1) {
-            // MongoDB에 저장?
-            System.out.println(userId+"가 "+alarmCode.name()+" : "+value+" 알람미 발송 실패했습니다.");
+            System.out.println("알림 생성 실패 - type 키 없음 : userId=" + userId);
+
+            // 필요하면 MongoDB fallback 저장 위치
+            // mongoAlarmFailRepository.save(...);
+
+            return; // 저장 안 함
+        }
+        try {
+            String jsonValue = objectMapper.writeValueAsString(values);
+
+            int result = alarmHistoryMapper.insertAlarmHistory(
+                    userId,
+                    alarmCode.name(),
+                    jsonValue
+            );
+
+            if (result != 1) {
+                System.out.println(userId + "가 " + alarmCode.name() + " : 알람 저장 실패");
+                // MongoDB 기록
+            }
+
+        } catch (JsonProcessingException e) {
+            System.out.println("알림 JSON 변환 실패 : " + e.getMessage());
         }
     }
-
 
 
 }
