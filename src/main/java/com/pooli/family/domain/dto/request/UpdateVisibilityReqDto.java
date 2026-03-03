57f55419cde2e3565a241c5827373496ab9875e1
hhsk908 <hhsk0908@naver.com>
2026-03-03 13:22:27 +0900

    fix(dto) : dto validation 및 오타 수정

--- src/main/java/com/pooli/family/domain/dto/request/UpdateVisibilityReqDto.java
+++ src/main/java/com/pooli/family/domain/dto/request/UpdateVisibilityReqDto.java
@@ -1,7 +1,7 @@
 package com.pooli.family.domain.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,11 +10,11 @@ import lombok.Setter;
 @Schema(description = "앱별 사용량 데이터 가족 공개 여부 설정 변경 요청 DTO")
 public class UpdateVisibilityReqDto {
 
-	@NotBlank
+	@NotNull
     @Schema(description = "회선 식별자", example = "10", requiredMode = Schema.RequiredMode.REQUIRED)
-    private Integer lineId;
+    private Long lineId;
 
-    @NotBlank
+    @NotNull
     @Schema(description = "가족 공개 여부", example = "true", requiredMode = Schema.RequiredMode.REQUIRED)
     private Boolean isPublic;
 }
