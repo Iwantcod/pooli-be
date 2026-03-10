213b0a6607cd2c81db512517d256f9670c1c3999
김준하 <kimjoonha0426@gmail.com>
2026-03-10 23:22:46 +0900

    Feat(traffic): 트래픽 처리 개발 1단계

--- src/main/java/com/pooli/common/config/SessionConfig.java
+++ src/main/java/com/pooli/common/config/SessionConfig.java
@@ -1,9 +1,11 @@
 package com.pooli.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
+@Profile({"default", "local", "api"})
 @EnableRedisHttpSession(redisNamespace = "${spring.session.redis.namespace}")
 public class SessionConfig {
 }
---
+++ src/main/java/com/pooli/common/config/StreamsRedisConfig.java
@@ -0,0 +1,39 @@
+package com.pooli.common.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
+
+@Configuration
+@Profile({"default", "local", "api", "traffic"})
+@EnableConfigurationProperties(StreamsRedisProperties.class)
+public class StreamsRedisConfig {
+
+    @Bean("streamsRedisConnectionFactory")
+    public RedisConnectionFactory streamsRedisConnectionFactory(StreamsRedisProperties properties) {
+        RedisStandaloneConfiguration configuration =
+                new RedisStandaloneConfiguration(properties.getHost(), properties.getPort());
+
+        if (StringUtils.hasText(properties.getPassword())) {
+            configuration.setPassword(RedisPassword.of(properties.getPassword()));
+        }
+
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean("streamsStringRedisTemplate")
+    public StringRedisTemplate streamsStringRedisTemplate(
+            @Qualifier("streamsRedisConnectionFactory") RedisConnectionFactory connectionFactory
+    ) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
+
