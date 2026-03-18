5994ff6db07356496a54dc7c60b1b7ed55a5c916
hyuuuun <pam4522@gmail.com>
2026-03-18 10:03:07 +0900

    fix : 관리자 역할 양도 500에러 수정

--- src/main/java/com/pooli/permission/service/RoleService.java
+++ src/main/java/com/pooli/permission/service/RoleService.java
@@ -1,9 +1,10 @@
 package com.pooli.permission.service;
 
+import com.pooli.auth.service.AuthUserDetails;
 import com.pooli.permission.domain.dto.response.RepresentativeRoleTransferResDto;
 
 public interface RoleService {
 
     // RY1-292: 대표자 권한 양도
-    RepresentativeRoleTransferResDto transferRepresentativeRole(Long currentLineId, Long changeLineId);
+    RepresentativeRoleTransferResDto transferRepresentativeRole(Long currentLineId, Long changeLineId, AuthUserDetails userDetails);
 }
